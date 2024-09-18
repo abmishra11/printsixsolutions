3b552fb81b343f7fb35339ac619367b221160c5d
Abhay Kumar Mishra <mishrakumar.abhay1@gmail.com>
2024-09-18 13:51:03 +0530

    made change in API function

--- app/(back-office)/dashboard/markets/new/page.jsx
+++ app/(back-office)/dashboard/markets/new/page.jsx
@@ -2,15 +2,17 @@ import NewMarketForm from "@/components/backoffice/NewMarketForm";
 import { getData } from "@/lib/getData";
 import React from "react";
 
-export default async function NewMarket() {
-  // Categories
+export default async function page() {
   const categoriesData = await getData("categories");
-  const categories = categoriesData.map((category) => {
-    return {
-      id: category.id,
-      title: category.title,
-    };
-  });
+  console.log("Fetched categories data:", categoriesData);
+
+  const categories =
+    categoriesData && categoriesData.length > 0
+      ? categoriesData.map((category) => ({
+          id: category.id,
+          title: category.title,
+        }))
+      : [];
 
   return <NewMarketForm categories={categories} />;
 }
