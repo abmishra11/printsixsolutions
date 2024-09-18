cbd21a8d00a2523cdf108731fc9114fda79e76c6
Abhay Kumar Mishra <mishrakumar.abhay1@gmail.com>
2024-09-18 14:21:03 +0530

    changed middleware function

--- middleware.js
+++ middleware.js
@@ -33,39 +33,43 @@ import { LOGIN, REGISTER, REGISTER_VENDOR, PUBLIC_ROUTES, ROOT } from "./lib/rou
 //     }
 // );
 
-export default async function middleware(req) {
-    const { nextUrl } = req;
-    const url = nextUrl.pathname;
-    
-    // Check if the route matches any of the public routes or the root
-    const isPublicRoute = PUBLIC_ROUTES.some(route => url.startsWith(route)) || nextUrl.pathname === ROOT;
-
-    const token = await getToken({ req, secret: process.env.NEXTAUTH_SECRET });
-    const role = token?.role;
-
-    // If the user is not authenticated and the route is not public, redirect to login
-    if (!role && !isPublicRoute) {
+export default withAuth(
+    async function middleware(req) {
+      const { nextUrl } = req;
+      const url = nextUrl.pathname;
+      
+      const isPublicRoute = PUBLIC_ROUTES.some(route => url.startsWith(route)) || nextUrl.pathname === ROOT;
+      
+      const token = req.nextauth.token;
+      const role = token?.role;
+  
+      // Redirect to login if not authenticated and not on a public route
+      if (!role && !isPublicRoute) {
         return NextResponse.redirect(new URL(LOGIN, req.url));
-    }
-
-    // Define role-based denied access rules
-    const deniedPaths = {
+      }
+  
+      const deniedPaths = {
         ADMIN: ["/checkout"],
         VENDOR: ["/dashboard/vendors", "/dashboard/categories", "/checkout"],
         USER: ["/dashboard/vendors", "/dashboard/customers", "/dashboard/sales", "/dashboard/categories", "/dashboard/products", "/dashboard/coupons", "/dashboard/vendor-support"]
-    };
-
-    // Determine if the user should be denied access to the requested page
-    const isDenied = (deniedPaths[role] || []).some(path => url.startsWith(path));
-
-    if (isDenied) {
+      };
+  
+      const isDenied = (deniedPaths[role] || []).some(path => url.startsWith(path));
+  
+      if (isDenied) {
         return NextResponse.redirect(new URL("/access-denied", req.url));
-    } else {
-        return NextResponse.next();
+      }
+  
+      return NextResponse.next();
+    },
+    {
+      callbacks: {
+        authorized: ({ token }) => !!token,
+      },
     }
-}
-
-export const config = {
+  );
+  
+  export const config = {
     matcher: ["/((?!api|_next/static|_next/image|.*\\.png$).*)", "/dashboard/:path*", "/checkout"]
-};
+  };
 
