cfde8c3ce11dbd68151cfdca830dd7248fc3e99c
Abhay Kumar Mishra <mishrakumar.abhay1@gmail.com>
2024-09-18 11:50:03 +0530

    removed backoffice

--- app/(back-office)/dashboard/orders/[id]/page.jsx
+++
@@ -1,8 +0,0 @@
-import OrderCard from "@/components/Order/OrderCard";
-import { getData } from "@/lib/getData";
-import React from "react";
-
-export default async function page({ params: { id } }) {
-  const order = await getData(`orders/${id}`);
-  return <OrderCard order={order} />;
-}
