5f63ae4af77df0f3c23e70512a901056938d56b8
Abhay Kumar Mishra <mishrakumar.abhay1@gmail.com>
2024-09-25 10:48:49 +0530

    fixed scroll issue

--- app/layout.js
+++ app/layout.js
@@ -12,6 +12,10 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en" className="dark">
+      <head>
+        <meta charSet="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+      </head>
       <body className={inter.className}>
         <Providers>
           {children}
